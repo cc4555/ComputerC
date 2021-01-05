83e36da34c418cde725d2637d47f62400ac1ec1c
cc4555 <48341923+cc4555@users.noreply.github.com>
2021-01-05 15:03:02 -0500

    Update Miner.lua

--- Miner.lua
+++ Miner.lua
@@ -153,13 +153,13 @@ function goMine()
             turnR()
 
             amount = mineZ - zPos
-            print("moving forward " .. amount)
+            print("moving forward " ..amount)
             for i=-20, amount do
                 forward()
             end
 
             print("moving down 22 blocks")
-            for i=1,21 do
+            for i=1,amount do
                 down()
             end
         end
