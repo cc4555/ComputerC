4c2277d3e2d25cb908272079a945eaf3c77d8ac3
cc4555 <48341923+cc4555@users.noreply.github.com>
2021-01-09 14:24:04 -0500

    Create Master.lua

---
+++ Master.lua
@@ -0,0 +1,17 @@
+rednet.open()
+
+while true do 
+    sInput = read()
+
+    if sInput == "calibrate" then
+        print("What computer would you like to calibrate")
+        sInput = read()
+        if sInput == "1" then
+            rednet.broadcast(226, "calibrate")
+        elseif sInput == "2" then
+            rednet.broadcast(227, "calibrate")
+        elseif sInput == "3" then
+            rednet.broadcast(228, "calibrate")
+        elseif sInput == "4" then
+            rednet.broadcast(231, "calibrate")
+        end
