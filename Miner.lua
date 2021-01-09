6010acfd8e1d48e88fb635f3942e0b002b36000f
cc4555 <48341923+cc4555@users.noreply.github.com>
2021-01-09 02:24:19 -0500

    Update Miner.lua

--- Miner.lua
+++ Miner.lua
@@ -1,5 +1,5 @@
-local xPos, yPos, zPos = nil
-face = 1
+Local xPos, yPos, zPos = nil
+face = 1 
 cal = false
 sInput = nil
 mineX = -1690
@@ -10,20 +10,15 @@ leftOffX = nil
 leftOffY = nil
 leftOffZ = nil
 
-
-function setLocation()
-    print("Getting location")
+function calibrate()
+    print("Calibrating")
     xPos, yPos, zPos = gps.locate()
-    print("Location set")
+    print("Finished")
     cal = true
 end
 
-function printLocation()
-    if cal == true then
-        print(xPos.." "..yPos.." "..zPos)
-    else
-        print("This miner needs to be calibrated")
-    end
+function setLocation()
+    xPos, yPos, zPos = gps.locate()
 end
 
 function turnL()
@@ -35,9 +30,9 @@ function turnL()
         turtle.turnLeft()
     elseif face == 2 then
         face = 3
-        turtle.turnLeft()
+        turle.turnLeft()
     elseif face == 3 then
-        face = 0
+        face = 4
         turtle.turnLeft()
     end
 end
@@ -52,7 +47,7 @@ function turnR()
     elseif face == 2 then
         face = 1
         turtle.turnRight()
-    elseif face == 3 then
+    elseif fasce == 3 then
         face = 2
         turtle.turnRight()
     end
@@ -61,17 +56,17 @@ end
 function forward()
     turtle.forward()
     if cal == true then
-     if face == 0 then
-      zPos = zPos - 1
-     elseif face == 1 then
-      xPos = xPos - 1
-     elseif face == 2 then
-      zPos = zPos + 1
-     elseif face == 3 then
-      xPos = xPos + 1
-     end
+        if face == 0 then
+            zPos = zPos + 1
+        elseif face == 1 then
+            xPos = xPos - 1
+        elseif face == 2 then
+            zPos = zPos - 1
+        elseif face == 3 then
+            xPos = xPos + 1
+        end
     else
-     print("Not Calibrated.")
+        print("Not Calibrated.")
     end
 end
 
@@ -101,210 +96,28 @@ function up()
     end
 end
 
-function down()
-    turtle.down()
-    if cal == true then
-        yPos = yPos - 1
-    else
-        print("Not Calibrated")
-    end
-end
-
-function refuel()
-    turtle.refuel()
-end
-
 function goMine()
-    print("Going to the Mine")
+    print("Going to the mine.")
 
     up()
 
     if zPos ~= mineZ then
-        if zPos > mineZ then --if the turtle is on the left side of the mine
-
-            turnR()
-
-            amount = zPos - mineZ
-            print("moving forward " ..amount)
-            for i=1,amount do
-                forward()
-            end
-
-            turnL()
-
-            amount = mineZ - zPos
-            print("moving forward " ..amount)
-            for i=-20,amount do
-                forward()
-            end
-
-            setLocation()
-
-            if yPos ~= mineY then
-                while yPos ~= mineY+1 do
-                    down()
-                    print(yPos)
-                end
-            end
-        end
-
-        if zPos < mineZ then --if the turtle is on the right side of the mine
-
-            turnL()
-
-            amount = mineZ - zPos
-            print("moving forward " ..amount)
-            for i=1,amount do
-                forward()
-            end
-
-            turnR()
-
-            amount = mineZ - zPos
-            print("moving forward " ..amount)
-            for i=-20, amount do
-                forward()
-            end
-
-            setLocation()
-
-            if yPos ~= mineY then
-                while yPos ~= mineY do
-                    down()
-                end
-            end
-
-            xPos, yPos, zPos = gps.locate()
-        end
-
         turnR()
 
-        for i=0,5 do
+        while zPos ~= mineZ do
             forward()
         end
+        
+        setLocation()
 
-        mine(5)
-
-    end
-end
-
-function mine(length)
-    for i=0,length do
-
-        turtle.dig()
-        if turtle.detect() then
-            while turtle.detect() == true do
-                turtle.dig()
-            end
-        end
-
-        forward()
-        turtle.digUp()
-        turtle.digDown()
-        turnL()
-
-        turtle.dig()
-        if turtle.detect() then
-            while turtle.detect() == true do
-                turtle.dig()
-            end
-        end
-
-        forward()
-        turtle.digDown()
-        turtle.digUp()
-        turnR()
-        turnR()
-        forward()
-
-        turtle.dig()
-        if turtle.detect() then
-            while turtle.detect() == true do
-                turtle.dig()
-            end
-        end
-        forward()
-        turtle.digDown()
-        turtle.digUp()
-        turnL()
-        turnL()
-        forward()
-        turnR()
-        xPos = xPos + 1
-    end
-end
-
-function invCheck()
-    for i=1,16 do
-        turtle.getItemSpace(i)
-        itemName = turtle.getItemDetail()
-        itemCount = turtle.getItemCount()
-
-        if turtle.getItemSpace(16) then
-            if turtle.getItemCount() == 64 then
-                goBack()
-            end
-        end
-    end
-end
-
-function goBack()
-    setLocation()
-    leftOffX = xPos
-    turnR()
-    turnR()
-    if xPos ~= mineX then
         while xPos ~= mineX do
             forward()
         end
-    end
-    
-    for i=0,20 do
-        up()
-    end
-
-    for i=0,18 do
-        forward()
-    end
-
-    turnL()
-
-    for i=0,20 do
-        forward()
-    end
-
-    for i=1,16 do
-        turtle.getSelectedSlot(i)
-        for i=1,64 do
-            turtle.dropDown()
-        end
-    end
-
-    turnR()
-    turnR()
-
-end
-
-while true do
-    sInput = read()
-    if sInput == "setLocation" or sInput == "set location" then
+        
         setLocation()
-    elseif sInput == "printLocation" or sInput == "print location" then
-        printLocation()
-    elseif sInput == "forward" then
-        forward()
-    elseif sInput == "refuel" then
-        refuel()
-    elseif sInput == "goMine" then
-        goMine()
-    elseif sInput == "mine" then
-        sInput = tonumber(read())
-        mine(sInput)
-    elseif sInput == "invCheck" then
-        invCheck()
-    elseif sInput == "goBack" then
-        goBack()
-    elseif sInput == "stop" then
-        break
+
+        while yPos ~= mineY do
+            down()
+        end
     end
 end
